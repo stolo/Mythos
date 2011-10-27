c18014ce55a1ea744a4e0d26d2ca70218901c2bf
stolar <stolar@linux-hv1d.site>
2011-10-26 23:36:37 -0400

    fixed Japanese typo

--- GetGods.py
+++ GetGods.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 from OlympiaNK import Olympus, God
+import sys
 
 YAML_FILE = './olympiansHR.yaml'
 olympus = Olympus()
@@ -43,4 +44,39 @@ def get_aztec():
         olympus.append(g)
         olympus.save_yaml(YAML_FILE)
 
-get_aztec()
+def get_chinese():
+    page = open("./NewChineseGods.data1", 'r')
+    for line in page.readlines():
+        name = line.split("|")[0].decode('utf-8')
+        description = line.split("|")[1].decode('utf-8')
+        domains = []
+        if len(line.split("|")) > 2:
+            domains_text = line.split("|")[2].decode('utf-8')
+            domains = domains_text.split(",")
+        pantheon = "Chinese"
+        g = God( { 
+                   'name' : name,
+                   'description': description,
+                   'pantheon' : pantheon,
+                   'domains' : domains 
+                 }
+               )
+        olympus.append(g)
+        print name, description, pantheon, domains
+    olympus.save_yaml(YAML_FILE)
+
+def get_gods():
+    page = open(sys.argv[1], 'r')
+    for line in page.readlines():
+        name = line.split(" | ")[0].decode('utf-8')
+        description = line.split(" | ")[1].decode('utf-8')
+        pantheon = sys.argv[2]
+        g = God( { 'name' : name,
+                   'description': description,
+                   'pantheon' : pantheon })
+        olympus.append(g)
+        print name
+        #print name, description, pantheon
+    olympus.save_yaml(YAML_FILE)
+
+get_chinese()
--- test.pl
+++ test.pl
@@ -1,23 +1,112 @@
 #!/usr/bin/perl
 
 use Lingua::LinkParser;
-use YAML;
+use Lingua::LinkParser::Definitions;
+use JSON;
 use strict;
 
 my $parser = new Lingua::LinkParser;        # create the parser
-my $zeus_text   = "Zeus is the God of thunder";
-my $thor_text   = "Thor is the lord of thunder";
+my @texts = ( "Hera is the goddess of birth", "Thor is the lord of thunder");
           
-my $sentence = $parser->create_sentence($zeus_text); # parse the sentence
-my $linkage  = $sentence->linkage(1);        # use the first linkage
-print $parser->get_diagram($linkage);         # print it out
 
-my $sentence = $parser->create_sentence($thor_text); # parse the sentence
-my $linkage  = $sentence->linkage(1);        # use the first linkage
-print $parser->get_diagram($linkage);         # print it out
+foreach my $text (@texts){
+    print $text . "\n";
+    my $linkage = get_linkage($text);
+    my @words = $linkage->words();
+    foreach my $word (@words){
+        print $word->{'_text'};
+        print "\n";
+        print get_word_link_codes($word);
+        print "\n";
+    }
+}
 
-my $greek_gods = YAML::LoadFile('./olympiansHR.yaml');
-#foreach (@greek_gods){
-#    print $_['name'];
+sub get_linkage(){
+    my $text = shift;
+    my $sentence = $parser->create_sentence($text); # parse the sentence
+    my $linkage  = $sentence->linkage(1);        # use the first linkage
+    return $linkage;
+}
+
+sub get_word_link_codes(){
+    my $word = shift;
+    my @links = $word->links;
+    my @link_codes;
+    foreach my $link (@links){
+        push (@link_codes, $link->{'linklabel'});
+    }
+    return @link_codes;
+}
+
+sub get_word_link_words(){
+    my $word = shift;
+    my @links = $word->links;
+    my @link_words;
+    foreach my $link (@links){
+        push (@link_words, $link->{'linkword'});
+    }
+    return @link_words;
+}
+
+
+#foreach my $word (@words){
+#    foreach my $key (keys %$word){
+#        print $key . " : ";
+#        print $word->{$key} . "\n";
+#    }
+#    my @links = $word->{"_links"};
+#    foreach my $link (@links){
+#        foreach my $l (@$link){
+#            foreach my $k (keys %$l){
+#                print $k . " : " . $l->{$k} . "\n";
+#            }
+#        }
+#    }
+#    print "***\n";
 #}
+#print $parser->get_diagram($linkage);         # print it out
 
+#my $sentence = $parser->create_sentence($thor_text); # parse the sentence
+#my $linkage  = $sentence->linkage(1);        # use the first linkage
+#print $parser->get_diagram($linkage);         # print it out
+
+#open( my $fh, '<', 'olympians.json' );
+#my $json_text   = <$fh>;
+#my $gods = decode_json( $json_text );
+#foreach (@$gods){
+#    my @description = split(/[\.\,\;\:]/, $_->{'description'});
+#    my $domains = $_->{'domains'};
+#    #print $domains; 
+#    my $rep;
+#    foreach my $desc(@description){
+#        foreach my $domain(@$domains){
+#            if ($desc =~ m/$domain/){
+#                my $sentence = $parser->create_sentence("$desc"); # parse the sentence
+#                my @linkages = $sentence->linkages;        # use the first linkage
+#                #print length(@linkages);
+#                my $rep;
+#                foreach my $linkage (@linkages) {
+#                    #print $parser->get_diagram($linkage);         # print it out
+#                    print define($linkage);
+#                    #print $parser->print_constituent_tree($linkage, 2);
+#                    #print $linkage->linklabel . "&&&&\n";
+#                    #my @words = $linkage->words;
+#                    #foreach my $word (@words){
+#                    #    print "$word->linkword\n";
+#                    #    foreach my $link ($word->links){
+#                    #        $rep .= $link->linklabel;
+#                    #    }
+#                    #}
+#                }
+#                if ($rep){
+#                    print "$desc\n";
+#                    print "$rep\n\n";
+#                }
+#            }
+#        }
+#    }
+#}
+#
+#my $sentence = $parser->create_sentence($text); # parse the sentence
+#my $linkage  = $sentence->linkage(1);        # use the first linkage
+#print $parser->get_diagram($linkage);         # print it out
